103db2ff19cb656ddff291048a71349fedc473b5
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-19 21:38:47 -0500

    Add Total Price info to OrderSummary component

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -100,7 +100,7 @@ class BurgerBuilder extends Component {
         return (
             <Wrapper>
                 <Modal show={this.state.checkingOut} modalClosed={this.purchaseCancelHandler}>
-                    <OrderSummary ingredients={this.state.ingredients} purchaseCancelled={this.purchaseCancelHandler} purchaseContinued={this.purchaseContinueHandler}/>
+                    <OrderSummary ingredients={this.state.ingredients} purchaseCancelled={this.purchaseCancelHandler} purchaseContinued={this.purchaseContinueHandler} price={this.state.totalPrice}/>
                 </Modal>
                 <Burger ingredients={this.state.ingredients} />
                 <BuildControls ingredientAdded={this.addIngredientHandler}
