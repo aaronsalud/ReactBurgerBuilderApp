07e9963443c280a8f5bb987df2546e53a1b2ee1c
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-20 15:17:43 -0500

    Render SideDrawerToggle component within the Toolbar

--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,10 +2,11 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import SideDrawerToggle from '../SideDrawer/SideDrawerToggle/SideDrawerToggle';
 
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
+        <SideDrawerToggle clicked={props.sideDrawerToggle}/>
         <div className={classes.Logo}>
             <Logo />
         </div>
