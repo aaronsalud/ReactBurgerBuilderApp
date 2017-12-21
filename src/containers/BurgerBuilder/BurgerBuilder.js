3ca3fd1e65e71e35d722f2f378da6a6867ffeb46
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-21 17:40:22 -0500

    Pass price info to query param

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -45,24 +45,12 @@ class BurgerBuilder extends Component {
     }
 
     purchaseContinueHandler = () => {
-        // this.setState({ loading: true });
-        // const order = {
-        //     ingredients: this.state.ingredients,
-        //     price: this.state.totalPrice,
-        // }
-        // axios.post('/orders.json', order)
-        //     .then(response => {
-        //         this.setState({ loading: false, checkingOut: false });
-        //     })
-        //     .catch(error => {
-        //         this.setState({ loading: false, checkingOut: false });
-        //     });
-
         const queryParams = [];
 
         for(let i in this.state.ingredients){
             queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
         }
+        queryParams.push('price=' + this.state.totalPrice);
 
         const queryString = queryParams.join('&');
 
