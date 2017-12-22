8e18b235bb9634399ee40c57f42630f6f7f38745
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-21 20:29:25 -0500

    Render Input Components

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -3,15 +3,63 @@ import Button from '../../components/UI/Button/Button';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
 import classes from './Contact.css';
+import Input from '../../components/UI/Input/Input';
 
 class Contact extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postal: ''
-        }
+        orderForm: {
+            name: {
+                type: 'input',
+                config: {
+                    type: 'text',
+                    placeholder: 'Your Name'
+                },
+                value: ''
+            },
+            email: {
+                type: 'input',
+                config: {
+                    type: 'text',
+                    placeholder: 'Your Email'
+                },
+                value: ''
+            },
+            street: {
+                type: 'input',
+                config: {
+                    type: 'text',
+                    placeholder: 'Your Street'
+                },
+                value: ''
+            },
+            postal: {
+                type: 'input',
+                config: {
+                    type: 'text',
+                    placeholder: 'Your Postal'
+                },
+                value: ''
+            },
+            country: {
+                type: 'input',
+                config: {
+                    type: 'text',
+                    placeholder: 'Your Country'
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                type: 'select',
+                config: {
+                    options: [
+                        {value: 'fastest', label: 'Fastest'},
+                        {value: 'cheapest', label: 'Cheapest'}
+                    ]
+                },
+                value: ''
+            }
+        },
+        loading: false
     }
 
     orderHandler = (event) => {
@@ -36,10 +84,10 @@ class Contact extends Component {
 
         let form = (
             <form action="">
-                <input className={classes.Input} type="text" name="name" placeholder="Your Name" />
-                <input className={classes.Input} type="email" name="email" placeholder="Your Email" />
-                <input className={classes.Input} type="text" name="street" placeholder="Your Stree" />
-                <input className={classes.Input} type="text" name="postal" placeholder="Your Postal" />
+                <Input inputtype="input" type="text" name="name" placeholder="Your Name" />
+                <Input inputtype="input" type="email" name="email" placeholder="Your Email" />
+                <Input inputtype="input" type="text" name="street" placeholder="Your Street" />
+                <Input inputtype="input" type="text" name="postal" placeholder="Your Postal" />
                 <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
             </form>
         );
