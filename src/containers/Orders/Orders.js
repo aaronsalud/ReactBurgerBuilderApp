5a4743711723ff5e559f2410cba99d2ff6082b5b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-26 18:19:38 -0500

    Implement Spinner while loading data

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -2,31 +2,45 @@ import React, { Component } from 'react';
 import Order from '../../components/Checkout/Order/Order';
 import axios from '../../axios-orders';
 import withErrorHandler from '../../components/HOC/withErrorHandler/withErrorHandler';
+import Spinner from '../../components/UI/Spinner/Spinner';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
 
+
 class Orders extends Component {
 
-    componentWillMount() {
+    componentDidMount() {
         this.props.onInitOrders();
     }
 
     render() {
-        return (
-            <div>
-                {
-                    this.props.orders.map(order => {
-                        return <Order key={order.id} ingredients={order.ingredients} price={order.price} />
-                    })
-                }
-            </div>
-        );
+
+        let ordersView = (
+        <div>
+            {
+                this.props.orders.map(order => {
+                    return <Order key={order.id} ingredients={order.ingredients} price={order.price} />
+                })
+            }
+        </div>);
+
+        if(this.props.loading){
+            ordersView = <Spinner />;
+        }
+
+        if(this.props.error){
+            ordersView = <p>ERROR: Failed to fetch orders data</p>
+        }
+
+        return (ordersView);
     }
 }
 
 const mapStateToProps = state => {
     return {
-        orders: state.order.orders
+        orders: state.order.orders,
+        loading: state.order.loading,
+        error: state.order.error
     };
 };
 
