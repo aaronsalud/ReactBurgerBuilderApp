8b0d0ced00e162b46358e50d622449ae798b927b
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-26 16:17:30 -0500

    Re map checkout props to combinereducers state

--- src/containers/Checkout/Checkout.js
+++ src/containers/Checkout/Checkout.js
@@ -36,8 +36,8 @@ class Checkout extends Component {
 
 const mapStateToProps = state => {
     return {
-        ingredients: state.ingredients,
-        totalPrice: state.totalPrice
+        ingredients: state.burgerBuilder.ingredients,
+        totalPrice: state.burgerBuilder.totalPrice
     }
 };
 
