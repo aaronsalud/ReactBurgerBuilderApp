d9c8ec6ea6a499065f4a5d064df4bcc4be9f5cba
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-26 15:30:50 -0500

    Add new action creator to broadcast loading event to the reducer method

--- src/store/actions/order.js
+++ src/store/actions/order.js
@@ -6,18 +6,25 @@ export const purchaseBurgerSuccess = (id, orderData) => {
         type: actionTypes.PURCHASE_BURGER_SUCCESS,
         orderId: id,
         orderData: orderData
-    }
-}
+    };
+};
 
 export const purchaseBurgerFailed = error => {
     return {
         type: actionTypes.PURCHASE_BURGER_FAILED,
         error: error
-    }
+    };
 }
 
-export const purchaseBurgerStart = orderData => {
+export const purchaseBurgerStart = () => {
+    return {
+        type: actionTypes.PURCHASE_BURGER_START
+    };
+};
+
+export const purchaseBurger = orderData => {
     return dispatch => {
+        dispatch(purchaseBurgerStart());
         axios.post('/orders.json', orderData)
             .then(response => {
                 dispatch(purchaseBurgerSuccess(response.data, orderData));
@@ -25,5 +32,5 @@ export const purchaseBurgerStart = orderData => {
             .catch(error => {
                 dispatch(purchaseBurgerFailed(error));
             });
-    }
-}
+    };
+};
