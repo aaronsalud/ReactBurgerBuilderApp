bf632e51ca3c2f60eb7640a9662f18fd70e94f43
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-27 22:15:34 -0500

    Add more validation rules within Contact Container

--- src/containers/Contact/Contact.js
+++ src/containers/Contact/Contact.js
@@ -112,6 +112,16 @@ class Contact extends Component {
             isValid = value.length <= rules.maxLength && isValid;
         }
 
+        if (rules.isEmail) {
+            const pattern = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
+            isValid = pattern.test(value) && isValid
+        }
+
+        if (rules.isNumeric) {
+            const pattern = /^\d+$/;
+            isValid = pattern.test(value) && isValid
+        }
+
         return isValid;
     }
 
