938cd4d9d9c3d23c5b81ff9857132b2ffb7e40c2
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-27 22:09:50 -0500

    Add method to validate form

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -37,6 +37,28 @@ class Auth extends Component {
         }
     };
 
+    checkValidity(value, rules) {
+        let isValid = true;
+
+        if (!rules) {
+            return isValid;
+        }
+
+        if (rules.required) {
+            isValid = value.trim() !== '' && isValid;
+        }
+
+        if (rules.minLength) {
+            isValid = value.length >= rules.minLength && isValid;
+        }
+
+        if (rules.maxLength) {
+            isValid = value.length <= rules.maxLength && isValid;
+        }
+
+        return isValid;
+    }
+
     render() {
 
         const formElementsArray = [];
