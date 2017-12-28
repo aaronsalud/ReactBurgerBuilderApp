03bb12220d8d69f900ae75c805ff5dfcbb540430
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-27 22:06:50 -0500

    Add new Auth container

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,74 @@
+import React, { Component } from 'react';
+import Input from '../../components/UI/Input/Input';
+import Button from '../../components/UI/Button/Button';
+import classes from './Auth.css';
+
+class Auth extends Component {
+    state = {
+        controls: {
+            email: {
+                type: 'input',
+                config: {
+                    type: 'email',
+                    placeholder: 'Mail Address'
+                },
+                validation: {
+                    required: true,
+                    isEmail: true
+                },
+                touched: false,
+                valid: false,
+                value: ''
+            },
+            password: {
+                type: 'input',
+                config: {
+                    type: 'password',
+                    placeholder: 'Password'
+                },
+                validation: {
+                    required: true,
+                    minLength: 6
+                },
+                touched: false,
+                valid: false,
+                value: ''
+            }
+        }
+    };
+
+    render() {
+
+        const formElementsArray = [];
+        for (let key in this.state.controls) {
+            formElementsArray.push(
+                {
+                    id: key,
+                    elementConfig: this.state.controls[key]
+                }
+            );
+        }
+
+        const form = formElementsArray.map(formElement => {
+            return <Input key={formElement.id}
+                elementType={formElement.elementConfig.type}
+                elementConfig={formElement.elementConfig.config}
+                value={formElement.elementConfig.value}
+                onChangeHandler={event => this.inputChangedHandler(event, formElement.id)}
+                invalid={!formElement.elementConfig.valid}
+                shouldValidate={formElement.elementConfig.validation}
+                touched={formElement.elementConfig.touched}
+            />
+        });
+        return (
+            <div className={classes.Auth}>
+                <form action="">
+                    {form}
+                    <Button btnType="Success">Submit</Button>
+                </form>
+            </div>
+        );
+    }
+}
+
+export default Auth;
