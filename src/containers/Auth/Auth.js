4318060b797d10dee5064062d0b4d1b8c06ea629
Aaron Mikhail Salud <mikhail.salud@gmail.com>
2017-12-29 18:40:33 -0500

    Add redirect to get users to the home route if logged in successfully

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
 import * as actions from '../../store/actions/index';
 import Input from '../../components/UI/Input/Input';
@@ -134,8 +135,15 @@ class Auth extends Component {
             );
         }
 
+        let authRedirect = null;
+
+        if (this.props.isAuthenticated) {
+            authRedirect = <Redirect to="/" />
+        }
+
         return (
             <div className={classes.Auth}>
+                {authRedirect}
                 {errorMsg}
                 <form onSubmit={this.submitHandler}>
                     {form}
@@ -151,7 +159,8 @@ class Auth extends Component {
 const mapStateToProps = state => {
     return {
         loading: state.auth.loading,
-        error: state.auth.error
+        error: state.auth.error,
+        isAuthenticated: state.auth.token !== null
     };
 };
 
